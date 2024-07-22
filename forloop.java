54c960a1d0afe5b7d6a16420eeaf2673436099ce
Usha Ramchandra Kudale <129097741+Ushakudale1511@users.noreply.github.com>
2024-07-22 21:20:44 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,11 @@
+//  syntax 
+//  for ( initilization; condition;updation){
+//     do something
+//  }
+class loops {
+    public static void main(String args []) {
+        for(int i = 0; i <= 10 ; i++) {
+           System.out.println (i);
+    }
+}
+}
